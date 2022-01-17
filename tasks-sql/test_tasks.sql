fecfe4d6aa70c189b34eb2b2b74021e73f66f1d4
Arsen Sattarov <arsen@MacBook-Pro-Arsen.local>
2022-01-17 15:58:49 +0300

    change sql solution

--- tasks-sql/test_tasks.sql
+++ tasks-sql/test_tasks.sql
@@ -1,29 +1,27 @@
 -- Найти самый дорогой товар. Вывести имя товара и его цену
-SELECT g.name, p.value AS price FROM Prices p
-JOIN Goods g ON p.goods_id = g.id
-ORDER BY p.value
-DESC
-LIMIT 1;
+SELECT Goods.name, Prices.value FROM Goods
+JOIN Prices ON Prices.goods_id = Goods.id
+WHERE Prices.value = (SELECT MAX(Prices.value) FROM Prices);
 
 -- Найти товары с нулевым остатком. Вывести имя товара и его цену
-SELECT g.name, p.value AS price FROM Prices p
-JOIN Goods g ON p.goods_id = g.id
-JOIN Quantity q ON q.goods_id = g.id
-WHERE q.value = 0;
+SELECT Goods.name, Prices.value AS price FROM Goods
+LEFT JOIN Prices ON Goods.id = Prices.goods_id
+LEFT JOIN Quantity ON Quantity.goods_id = Goods.id
+WHERE Quantity.value = 0 OR Quantity.value IS NULL;
 
 -- Найти производителя с самой большой средней ценой за товары. Вывести имя производителя и среднюю стоимость
-SELECT m.name, ROUND(AVG(p.value), 2) AS average_price FROM Prices p
-JOIN Goods g ON p.goods_id = g.id
-JOIN Suppliers s ON s.id = g.supplier_id
-JOIN Manufacturer m ON m.id = s.manufacturer_id
-GROUP BY m.name
-ORDER BY AVG(p.value)
+SELECT Manufacturer.name, ROUND(AVG(Prices.value), 2) AS average_price FROM Prices
+JOIN Goods ON Goods.id = Prices.goods_id
+JOIN Suppliers ON Suppliers.id = Goods.supplier_id
+JOIN Manufacturer ON Manufacturer.id = Suppliers.manufacturer_id
+GROUP BY Manufacturer.name
+ORDER BY AVG(Prices.value)
 DESC
 LIMIT 1;
 
 -- Найти все товары производителей из Москвы. Вывести имена товаров, их цены и имена производителей
-SELECT g.name, p.value AS price, m.name AS manufacturer FROM Prices p
-JOIN Goods g ON p.goods_id = g.id
-JOIN Suppliers s ON s.id = g.supplier_id
-JOIN Manufacturer m ON m.id = s.manufacturer_id
-WHERE m.location = 'Moscow';
+SELECT Goods.name, Prices.value AS price, Manufacturer.name AS manufacturer FROM Prices
+JOIN Goods ON Goods.id = Prices.goods_id
+JOIN Suppliers ON Suppliers.id = Goods.supplier_id
+JOIN Manufacturer ON Manufacturer.id = Suppliers.manufacturer_id
+WHERE Manufacturer.location = 'Moscow';
